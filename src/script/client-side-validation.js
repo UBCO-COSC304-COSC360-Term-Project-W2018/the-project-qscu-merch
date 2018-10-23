7231ce417eb8e4c8f38c93c59282f182feccb8fe
Rachelle Gelden <rachelle@gelden.com>
2018-10-23 16:34:08 -0700

    Just gotta commit and push to master before I can start on my own branch

--- src/script/client-side-validation.js
+++ src/script/client-side-validation.js
@@ -4,15 +4,9 @@ window.onload = function () {
 
     document.getElementById("loginForm").onsubmit = function (e) {
 
-        //login form elements that must be validated
         var emailField = document.getElementById("loginEmailInput");
         var passwordField = document.getElementById("loginPasswordInput");
 
-        // if ( !validateEmail(loginEmailField) ) {
-        //     e.preventDefault();
-        //     alert("please enter a valid email");
-        // }
-        //
         if ( passwordField.value == "" ) {
             e.preventDefault();
             alert("Please enter a password");
@@ -20,16 +14,7 @@ window.onload = function () {
 
     };
 
-    /*
-    regular expression from https://flaviocopes.com/how-to-validate-email-address-javascript/
-     */
-    // function validateEmail ( email ) {
-    //     var expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    //
-    //     return expression.test(String(email).toLowerCase());
-    // }
-
-    document.getElementById("signUpForm").onsubmit = function (e) {
+       document.getElementById("signUpForm").onsubmit = function (e) {
 
         //sign-up form elements to be validated
         var fnamefield = document.getElementById("fnameInput");
