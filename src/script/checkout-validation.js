8964c75fee0dde216d5c0dc966661a2a930daba9
Rachelle Gelden <rachelle@gelden.com>
2018-10-24 19:21:13 -0700

    alright here is some validation :')

--- src/script/checkout-validation.js
+++ src/script/checkout-validation.js
@@ -1,28 +1,42 @@
 window.onload = function () {
 
+$("#shippingAddressContainer").on('change',function() {
+
+})
+
     document.getElementById("checkOutForm").onsubmit = function (e) {
 
-        e.preventDefault();
+        /*
+        validates date
+         */
         var date = document.getElementById("ccExpiration");
         if ( !validDate(date) ) {
             e.preventDefault();
             alert("please enter valid date in valid form: MMYY");
         }
 
-        var radio1 = document.getElementById("addressOnFileRadio");
-        var radio2 = document.getElementById("newAddressRadio");
+        /*
+        make sure a radio button is checked
+         */
+        // var radio1 = document.getElementById("billingAddressRadio");
+        var radio2 = document.getElementById("shippingAddressRadio");
 
-        if ( !validRadioGroup( radio1, radio2 )) {
+        // if ( !validRadioGroup( radio1, radio2 )) {
+        //     e.preventDefault();
+        //     alert("Please select a shipping address");
+        // }
+
+        if ( radio2.checked ) {
             e.preventDefault();
-            alert("Please select a shipping address");
+            document.getElementById("shippingAddressContainer").style.display = "show";
+            validateNewShippingAddress();
         }
 
         var ccNum = document.getElementById("ccNum");
         if ( ccNum < 0 ) {
             e.preventDefault();
+            alert("please enter a valid ccv")
         }
-
-
     }
 
     function validDate ( givenDate ) {
@@ -54,11 +68,32 @@ window.onload = function () {
     function validRadioGroup ( rb1, rb2 ) {
 
         var radiogrouptest = true;
+        console.log("entered method")
 
         if ( !( rb1.checked || rb2.checked ) ) {
             radiogrouptest = false;
+            console.log("no radio selected");
         }
 
         return radiogrouptest;
+    }
+
+    /*
+    validate new shipping address fields
+     */
+    function validateNewShippingAddress () {
+
+        document.getElementById("shippingAddressInput").required = true;
+        document.getElementById("shippingCityInput").required=true;
+        document.getElementById("shippingProvinceInput").required=true;
+        document.getElementById("shippingCountryInput").required = true;
+
+    }
+
+    function validString ( string ) {
+        if ( string == "" || /^$|\s+/.test(string) ) {
+            return false;
+        }
+        else return true;
     }
 }
