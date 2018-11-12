6c6c4b8cf37a2a0da596494d0a2a9d93efda63fb
Brandon <funtime.spik330@gmail.com>
2018-11-12 09:46:58 -0800

    git inclues

--- src/includes/session.php
+++ src/includes/session.php
@@ -4,8 +4,6 @@ session_start();
 $cookie_name = "name";
 $cookie_value = "temp";
 
-$_SESSION["userId"] = 0;
-
 if(!isset($_COOKIE)) {
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); //
 }
