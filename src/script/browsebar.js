c1758e24cc493e1ea01e62c033bd53df61ae745d
jameskoss14 <43121875+jameskoss14@users.noreply.github.com>
2018-11-11 17:08:42 -0800

    finished browse button drop down menu

--- src/script/browsebar.js
+++ src/script/browsebar.js
@@ -4,31 +4,34 @@ var browsebar;
 		
 	$("#browsebutton")
 		.on("click", function(e){
-			browsebar = $('<div id="browsedrop"></div>');
-			var browsenav = $('<nav id="browsenav"></nav>');
-			var browsetitle = $('<h4 id="browsetitle">Categories</h4>');
-			var browselist = $('<ul id="browselist"></ul>');
+			browsebar = document.getElementById("browsesection");
+			var browsenav = $('<nav class="added" id="browsenav"></nav>');
+			var browsetitle = $('<h4 class="added" id="browsetitle">Categories</h4>');
+			var browselist = $('<ul class="added" id="browselist"></ul>');
 			for(var i = 0; i<9; i++){
-				var browseitem = $('<li class="browseitem"><a href="categorypage.html" class="browselink">Filler Name</a></li>');
+				var browseitem = $('<li class="added" class="browseitem"><a href="categorypage.html" class="added" class="browselink">Filler Name</a></li>');
 				$(browselist).append(browseitem);
 			} //eventually loop so it pulls the category names from our database
 			$(browsetitle).appendTo(browsenav);
 			$(browselist).appendTo(browsenav);
-			$(browsenav).appendTo(browsebar);
+			$(browsenav).appendTo('#browsesection');
 			
-			browsebar.appendTo($("header"));
-			
-			browsebar.fadeIn(1000);
-			browsebar.css('position', 'fixed');
-			browsebar.css("top", ($("header")).height());
-			browsebar.css("left", "0px");
+			browsenav.fadeIn(1000);
+			browsenav.css('position', 'absolute');
+			browsenav.css("top", ($("header")).height());
+			browsenav.css("left", "0px");
 	
-			})
-			
-	$(browsebar)
-		.on("click", function(){
-			console.log("hi");
-			//browsebar.remove();
 			});
+			
+		
+		$(document).mouseup(function(e){
+			console.log("heythere");
+			var container = $("#browsenav");
+    		if (!container.is(e.target) && container.has(e.target).length === 0) 
+				{
+					container.remove();
+				}
+    	
+    	});
 			
 });
