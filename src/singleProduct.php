f9d9c52b544e7fd8e63623d7de6ee6b625f54248
Parsa <ParsaRajabiPR@gmail.com>
2018-11-12 11:58:18 -0800

    working on singleProduct

--- src/singleProduct.php
+++ src/singleProduct.php
@@ -1,8 +1,15 @@
-<?php 
+<?php
 include "init.php";
 include "header.php";
-?>
 
+
+$con = mysqli_connect("cosc360.ok.ubc.ca", "40215162", "password", "db_402151562");
+
+if ( mysqli_connect_errno()) {
+echo "Failed to connect to MySQL";
+}
+
+?>
     <!--    make sure all the files that we update have the same and CORRECT width/height -->
     <div class="bigboi">
         <div class="container">
@@ -37,7 +44,7 @@ include "header.php";
                 <div class="quant">
                     <p>Quantity</p>
                     <form id='myform' method='POST' action='#'>
-                        <!--                    TODO: need to send this somwehere-->
+                        <!--TODO: need to send this somwehere-->
                         <input type='button' value='-' class='qtyminus' field='quantity' />
                         <input type='text' name='quantity' value='0' class='qty' />
                         <input type='button' value='+' class='qtyplus' field='quantity' />
