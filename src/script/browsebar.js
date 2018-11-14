22126d50e7b2ac45abec6125a874a74390464d05
jameskoss14 <43121875+jameskoss14@users.noreply.github.com>
2018-11-14 11:41:41 -0800

    css for profile page

--- src/script/browsebar.js
+++ src/script/browsebar.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
 var browsebar;		
-		
+var isOpen = false;
+
+
 	$("#browsedropbutton")
 		.on("click", function(e){
+			console.log(isOpen);
+			if(!isOpen){
+				isOpen=true;
 			var browsedropnav = $('<nav id="browsedropnav"></nav>');
 			var browsedroptitle = $('<h4 id="browsedroptitle">Categories</h4>');
 			var browsedroplist = $('<ul id="browsedroplist"></ul>');
@@ -19,17 +24,25 @@ var browsebar;
 			browsedropnav.css('position', 'absolute');
 			browsedropnav.css("top", ($("header")).height());
 			browsedropnav.css("left", "0px");
-	
-			});
 			
-		
-		$(document).mouseup(function(e){
-			var container = $("#browsedropnav");
-    		if (!container.is(e.target) && container.has(e.target).length === 0) 
+			}else{
+				var container = $("#browsedropnav");
+					if (!container.is(e.target) && container.has(e.target).length === 0&&isOpen) 
 				{
 					container.remove();
+					isOpen=false;
 				}
-    	
-    	});
+				
+			}
+			});
 			
+
+		$(document).mouseup(function(e){
+			var container = $("#browsedropnav");
+    		if (!container.is(e.target) && container.has(e.target).length === 0 &&isOpen) 
+				{
+					container.remove();
+					
+				}
+    	});
 });
