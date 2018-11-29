009f26d3aac3cd87f278c8e1038a31e160629a8c
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2018-11-29 14:31:12 -0800

    added error404.php

---
+++ src/error404.php
@@ -0,0 +1,47 @@
+<?php
+include "init.php";
+include "header.php";
+?>
+<!DOCTYPE HTML>
+
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/about.css"/>
+    <title>QSCU Merch Store</title>
+    <?php include 'includes/headerFooterHead.php'; ?>
+    <link rel="icon" type="image/x-icon" href="../src/images/QSCU_favicon.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+<body>
+
+<div id="content" class="error404">
+
+    <div id="post-0">
+
+        <h1 class="error-404-title entry-title">PAGE NOT FOUND (ERROR 404)</h1>
+
+        <div class="entry-content">
+            <p>Oops, the page you are looking for is not available. Sorry about that.</p>
+
+            <img style="max-width: 50%;" src="../resources/404-shrug.gif">
+
+            Go back to the <a href="/">home page</a>. <br><br>
+
+        </div><!-- .entry-content -->
+
+    </div><!-- .hentry -->
+
+</div>
+
+</body>
+
+</html>
+
+<?php
+include "footer.php";
+?>
+
+
