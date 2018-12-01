920a486e93478bc7fca1e95962bbf3fd4fe09d70
Parsa <ParsaRajabiPR@gmail.com>
2018-11-30 20:10:15 -0800

    added Chart using phpCharts

--- src/displayInventory.php
+++ src/displayInventory.php
@@ -1,5 +1,6 @@
 <?php
-$headerSet = 1;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include "includes/init.php";
 include "includes/validateAdmin.php";
 
@@ -25,49 +26,10 @@ if($con -> connect_errno){
 <head lang="en">
     <meta charset="utf-8">
     <title>QSCU Merch Store</title>
-    <link rel="stylesheet" href="css/home.css"/>
     <link rel="stylesheet" href="css/displayInventory.css"/>
     <?php include 'includes/headerFooterHead.php'?>
 
 
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-
-        // Load the Visualization API and the corechart package.
-        google.charts.load('current', {'packages':['corechart']});
-
-        // Set a callback to run when the Google Visualization API is loaded.
-        google.charts.setOnLoadCallback(drawChart);
-
-        // Callback that creates and populates a data table,
-        // instantiates the pie chart, passes in the data and
-        // draws it.
-        function drawChart() {
-
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            // data.addColumn('number', )
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-                ['Mushrooms', 3],
-                ['Onions', 1],
-                ['Olives', 1],
-                ['Zucchini', 1],
-                ['Pepperoni', 2]
-            ]);
-
-            // Set chart options
-            var options = {'title':'How Much Pizza I Ate Last Night',
-                'width':400,
-                'height':300};
-
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
-        }
-    </script>
 
 </head>
 
@@ -81,9 +43,20 @@ if($con -> connect_errno){
 	<p id="warehousepagetitle">Warehouse Product Inventory List <a id='btn' href="../src/adminList.php">Return To Admin</a></p>
     <!--Div that will hold the pie chart-->
     <div id="container">
-    <div id="chart_div"></div>
+        <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+<!--        <div id="chart_div"></div>-->
 <?php
 try {
+    $inventoryProduct = array();
+//        array("label"=> $pname, "y"=> $quantity),
+//        array("label"=> "Activities and Entertainments", "y"=> 261),
+//        array("label"=> "Health and Fitness", "y"=> 158),
+//        array("label"=> "Shopping & Misc", "y"=> 72),
+//        array("label"=> "Transportation", "y"=> 191),
+//        array("label"=> "Rent", "y"=> 573),
+//        array("label"=> "Travel Insurance", "y"=> 126)
+//    );
 
 
     $sqlWH = "SELECT wNo, location FROM Warehouse";
@@ -116,13 +89,23 @@ try {
 
                 $prods->bind_result($pNo, $size, $quantity, $pname);
 
+
                 while ($prods->fetch()) {
+//                    $itemName = array('pno'=>$pNo, 'pname'=>$pname, 'qty'=> $quantity);
+//                    array("label"=> "Food + Drinks", "y"=> 590),
+//                    $itemName = array('pno'=>$pNo, 'pname'=>$pname, 'qty'=> $quantity);
+                    $itemName = array("label"=>$pname, "y"=>$quantity);
+                    array_push($inventoryProduct, $itemName);
+
+//                    array_push($inventoryProduct, $itemName);
 
                     echo "<tr><td>" . $pNo . "</td>";
                     echo "<td>" . $pname . "</td>";
                     echo "<td>" . $size . "</td>";
                     echo "<td>" . $quantity . "</td></tr>";
                 }
+//                $inventoryProduct = array("label"=>$pname, "y"=>$quantity);
+
 
 
             } else {
@@ -152,6 +135,80 @@ catch(Exception $e) {
 <?php
 include "footer.php";
 ?>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script>
+    window.onload = function () {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            exportEnabled: true,
+            title:{
+                text: "QSCU Product/Price Chart"
+            },
+            subtitles: [{
+                text: "Currency Used: Thai Baht (฿) - use the top right button to save as PNG/JPG"
+            }],
+            data: [{
+                type: "pie",
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabelFontSize: 16,
+                indexLabel: "{label} - #percent%",
+                yValueFormatString: "฿#,##0",
+                dataPoints:
+                <?php echo json_encode($inventoryProduct, JSON_NUMERIC_CHECK); ?>
+            }]
+        });
+        chart.render();
+
+    }
+</script>
 
 </body>
+<!---->
+<!--Load the AJAX API-->
+<!--<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>-->
+<!--<script type="text/javascript">-->
+<!---->
+<!--    // Load the Visualization API and the corechart package.-->
+<!--    google.charts.load('current', {'packages':['corechart']});-->
+<!---->
+<!--    // Set a callback to run when the Google Visualization API is loaded.-->
+<!--    google.charts.setOnLoadCallback(drawChart);-->
+<!---->
+<!--    // Callback that creates and populates a data table,-->
+<!--    // instantiates the pie chart, passes in the data and-->
+<!--    // draws it.-->
+<!--    function drawChart() {-->
+<!---->
+<!--        // Create the data table.-->
+<!--        var data = new google.visualization.DataTable();-->
+<!--        // data.addColumn('number', )-->
+<!---->
+<!--        var inventoryName = $("#inventoryStatusPHP").val();-->
+<!--        var inventoryQuantity = $("#inventoryStatusPHP.qty").val();-->
+<!---->
+<!--        data.addColumn('string', 'Topping');-->
+<!--        data.addColumn('number', 'Slices');-->
+<!--        data.addRow([inventoryName.val, inventoryQuantity]);-->
+<!---->
+<!--        //-->
+<!--        // data.addRows([-->
+<!--        //     ['Mushrooms', 3],-->
+<!--        //     ['Onions', 1],-->
+<!--        //     ['Olives', 1],-->
+<!--        //     ['Zucchini', 1],-->
+<!--        //     ['Pepperoni', 2]-->
+<!--        // ]);-->
+<!---->
+<!--        // Set chart options-->
+<!--        var options = {'title':'QSCU Store Inventory',-->
+<!--            'width':700,-->
+<!--            'height':500};-->
+<!---->
+<!--        // Instantiate and draw our chart, passing in some options.-->
+<!--        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));-->
+<!--        chart.draw(data, options);-->
+<!--    }-->
+<!--</script>-->
 </html>
