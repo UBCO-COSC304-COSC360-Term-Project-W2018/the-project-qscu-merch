ab97b3209948931b90bf4c38d3561345cf65b2e1
Parsa <ParsaRajabiPR@gmail.com>
2018-11-30 22:25:21 -0800

    fixed db_cred

--- src/includes/db_credentials.php
+++ src/includes/db_credentials.php
@@ -1,13 +1,10 @@
 <?php
 if(!isset($dbcred)){
     $dbcred = true;
-//    define("DBHOST","cosc304.ok.ubc.ca");
-//    define("DBNAME", "db_rgelden");
-//    define("DBUSER", "rgelden");
-//    define("DBPASS", "40215162");
-    define("DBHOST","localhost");
-    define("DBNAME", "qscurachelle");
-    define("DBUSER", "rachellegelden");
-    define("DBPASS", "rachelle");
+    define("DBHOST","cosc304.ok.ubc.ca");
+    define("DBNAME", "db_rgelden");
+    define("DBUSER", "rgelden");
+    define("DBPASS", "40215162");
+
 }
 ?>
